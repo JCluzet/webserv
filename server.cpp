5ef98f0ec6f1513896d89b585fab1f0e02bb51ff
JCluzet <jo@cluzet.fr>
2022-04-25 22:50:51 +0200

    push

--- server.cpp
+++ server.cpp
@@ -7,20 +7,31 @@
 #include <string.h>
 #include <fstream>
 #include <iostream>
+#include <sys/types.h>
 #include <string>
+#include <sys/stat.h>
+#include <fstream>
 
-bool    readFile(const char * filename, std::string *fileContent) {
+
+bool    readFile(std::string filename, std::string *fileContent) {
     std::string     s;
     std::ifstream   ifs(filename);
 
     if (!ifs) {
-        std::cerr << "Can't open " << filename << "." << std::endl;
+        std::cerr << "Not Found " << filename << "." << std::endl;
+        *fileContent = "\n<!DOCTYPE html>\n\n<html>\n\n<body>\n  \n  <h1>ERROR 404</h1>\n    <p>File not found.</p>\n</body>\n\n</html>";
         return (1);
     }
+    // if filename is a folder, set it to index.html ( detect by using is_directory() )
+    // if (is_directory(filename)) {
+    //     filename += "index.html";
+    // }
     getline(ifs, s);
     if (s == "") {
         std::cerr << "Empty file." << std::endl;
         ifs.close();
+        *fileContent = "\n";
+        *fileContent = "<!DOCTYPE html>\n\n<html>\n\n<body>\n  \n  <h1>ERROR 404</h1>\n    <p>Empty file.</p>\n</body>\n\n</html>";
         return (1);
     }
     *fileContent += s;
@@ -31,6 +42,34 @@ bool    readFile(const char * filename, std::string *fileContent) {
     }
     ifs.close();
     return (0);
+}
+
+std::string cut_aftercomma(std::string s)
+{
+    int i = 0;
+    while (s[i] != ',')
+        i++;
+    return (s.substr(0, i));
+}
+
+std::string getContentType(std::string client_data)
+{
+    std::string content_type = "";
+    for (unsigned int i = 0; i < client_data.length() - 7; i++)
+    {
+        if (client_data[i] == 'A' && client_data[i + 1] == 'c' && client_data[i + 2] == 'c' && client_data[i + 3] == 'e' && client_data[i + 4] == 'p' && client_data[i + 5] == 't' && client_data[i + 6] == ':')
+        {
+            i += 8;
+            while (client_data[i] != ',')
+            {
+                content_type += client_data[i];
+                i++;
+            }
+            break;
+        }
+    }
+    std::cout << "Content-Type:" << content_type << std::endl;
+    return (content_type);
 }
 
 std::string getFile(std::string client_data)
@@ -49,17 +88,32 @@ std::string getHeader(std::string client_data)
 {
   std::string response = "HTTP/1.1 200 OK";
   response += "\nContent-Type: ";
-  response += "text/html"; // -->> need to detect the content type
+  response += getContentType(client_data);
+  // response += "text/html"; // -->> need to detect the content type
   response += "\nContent-Length: ";
   return(response);
 }
+
+#include <sys/types.h>
+
+std::string set_default_page(std::string filetosearch, std::string client_data)
+{
+    if(filetosearch[filetosearch.length() - 1] == '/')
+    {
+      filetosearch += "index.html";                                             // <-- replace here the default page in config file
+      std::cout << "setting DEFAULT to index.html" << std::endl;
+    }
+    return (filetosearch);
+}
+
 
 std::string data_sender(std::string client_data)
 {
   std::string response;
   std::string filecontent;
   response = getHeader(client_data);
-  std::string filetosearch = "root" + getFile(client_data);
+  std::string filetosearch = "root" + getFile(client_data);                       // <-- replace "root" here with config file
+  filetosearch = set_default_page(filetosearch, client_data);
   std::cout << "filetosearch>>" << filetosearch << "<<" << std::endl;
   readFile(filetosearch.c_str(), &filecontent);
   response += std::to_string(filecontent.length());              // fill the content-lenght header part
@@ -135,18 +189,15 @@ int main(int argc, char const *argv[])
             perror("In accept");
             exit(EXIT_FAILURE);
         }
-        // response = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: ";
         
         char client_data[30000] = {0};
         valread = read( new_socket , client_data, 30000);
-        printf(">>%s<<\n",client_data);
+        printf("RECEIVING:\n%s\n",client_data);
         std::string file;
 
         response = data_sender(client_data);
-        // response += std::to_string(filecontent.length());
-        std::cout << "<<< " << response << std::endl;
+        std::cout << "\n\nOUR RESPONSE: " << std::endl << response << std::endl;
         write(new_socket , response.c_str() , response.length());
-        // printf("------->   message sent : \n%s <-------", response.c_str());
         close(new_socket);
     }
     return 0;
