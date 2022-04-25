b11f2d7a4bbe20b8553382b72b26ba1bec569288
JCluzet <jo@cluzet.fr>
2022-04-25 19:01:25 +0200

    push

--- server.cpp
+++ server.cpp
@@ -1,83 +1,63 @@
-#include <sys/socket.h> // For socket functions
-#include <netinet/in.h> // For sockaddr_in
-#include <cstdlib> // For exit() and EXIT_FAILURE
-#include <iostream> // For cout
-#include <unistd.h> // For read
+// Server side C program to demonstrate HTTP Server programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <iostream>
 
-int main() {
-  // Create a socket (IPv4, TCP)
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd == -1) {
-    std::cout << "SERVER: Failed to create socket. errno: " << errno << std::endl;
-    exit(EXIT_FAILURE);
-  }
-
-  printf("SERVER: Socket created with sockfd: %d\n", sockfd);
-
-  std::cout << "SERVER: Enter port number: ";
-  int port;
-  std::cin >> port;
-
-
-  // Listen to port 9999 on any address
-  sockaddr_in sockaddr;
-  sockaddr.sin_family = AF_INET;
-  sockaddr.sin_addr.s_addr = INADDR_ANY;
-  sockaddr.sin_port = htons(port); // htons is necessary to convert a number to
-                                   // network byte order
-  if (bind(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0) {
-    std::cout << "SERVER: Failed to bind to port " << port << "." << "errno : " << errno << std::endl;
-    exit(EXIT_FAILURE);
-  }
-
-  std::cout << "SERVER: Listening on port  " << port << " setting 10 place max in the queue" << "..." << std::endl;
-
-  // Start listening. Hold at most 10 connections in the queue
-  if (listen(sockfd, 10) < 0) {
-    std::cout << "SERVER: Failed to listen on socket. errno: " << errno << std::endl;
-    exit(EXIT_FAILURE);
-  }
-
-  std::cout << "SERVER: Waiting for incoming connections..." << std::endl;
-
-  // Grab a connection from the queue
-  auto addrlen = sizeof(sockaddr);
-  int connection = accept(sockfd, (struct sockaddr*)&sockaddr, (socklen_t*)&addrlen);
-  if (connection < 0) {
-    std::cout << "SERVER: Failed to grab connection. errno: " << errno << std::endl;
-    exit(EXIT_FAILURE);
-  }
-
-  std::cout << "SERVER: Got a connection" << std::endl;
-
-  // Read from the connection
-  char buffer[100];
-  auto bytesRead = read(connection, buffer, 100);
-  std::cout << "CLIENT: " << buffer;
-
-  // Send a message to the connection
-  std::string response = "SERVER:Good talking to you\n";
-  send(connection, response.c_str(), response.size(), 0);
-
-  // Close the connections
-  while (true) {
-    close(connection);
-    connection = accept(sockfd, (struct sockaddr*)&sockaddr, (socklen_t*)&addrlen);
-    if (connection < 0) {
-      std::cout << "SERVER: Failed to grab connection. errno: " << errno << std::endl;
-      exit(EXIT_FAILURE);
+#define PORT 8585
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket; long valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    
+    // Only this line has been changed. Everything is same.
+    std::string hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+    // std::string hello = "HTTP/1.1 200 OK\nDate: Fri, 16 Mar 2018 17:36:27 GMT\nserver: myServerContent-Type: text/html;charset=UFT-8\nContent-Length: 1846\n\n<?xml>\n<!DOCTYPE html>\n<html>\n<body>\n\n<h1>My First Heading</h1>\n<p>My first paragraph.</p>\n\n</body>\n</html>\n";
+ 
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("In socket");
+        exit(EXIT_FAILURE);
     }
-    std::cout << "SERVER: Got a connection" << std::endl;
+    
 
-    // Read from the connection
-    char buffer[100];
-    auto bytesRead = read(connection, buffer, 100);
-    std::cout << "CLIENT: " << buffer;
-
-    // Send a message to the connection
-    std::string response = "SERVER:Good talking to you\n";
-    send(connection, response.c_str(), response.size(), 0);
-  }
-  close(connection);
-  close(sockfd);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+    
+    memset(address.sin_zero, '\0', sizeof address.sin_zero);
+    
+    
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+    {
+        perror("In bind");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 10) < 0)
+    {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+    while(1)
+    {
+        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+        
+        char buffer[30000] = {0};
+        valread = read( new_socket , buffer, 30000);
+        printf("%s\n",buffer );
+        write(new_socket , hello.c_str() , hello.length());
+        printf("------------------Hello message sent-------------------");
+        close(new_socket);
+    }
+    return 0;
 }
