0ee91676242524eae3e27f45aef8fe4d969776d1
JCluzet <jo@cluzet.fr>
2022-04-25 21:03:04 +0200

    AHOUIIIII

--- server.cpp
+++ server.cpp
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <fstream>
 #include <iostream>
+#include <string>
 
 bool    readFile(const char * filename, std::string *fileContent) {
     std::string     s;
@@ -32,15 +33,41 @@ bool    readFile(const char * filename, std::string *fileContent) {
     return (0);
 }
 
-std::string data_sender(std::string file)
+std::string getFile(std::string client_data)
 {
-  std::string data = "HTTP/1.1 200 OK\n";
-  data += "Content-Type: text/html\n"; // --> check the content-type
-  data += "Content-Length: 12\n\n"; 
-  return(data);
+  std::string file;
+  for (int i = 4; i < client_data.length(); i++)
+  {
+    if (client_data[i] == ' ')
+      break;
+    file += client_data[i];
+  }
+  return (file);
 }
 
-// #define PORT 8585
+std::string getHeader(std::string client_data)
+{
+  std::string response = "HTTP/1.1 200 OK";
+  response += "\nContent-Type: ";
+  response += "text/html"; // -->> need to detect the content type
+  response += "\nContent-Length: ";
+  return(response);
+}
+
+std::string data_sender(std::string client_data)
+{
+  std::string response;
+  std::string filecontent;
+  response = getHeader(client_data);
+  std::string filetosearch = "root" + getFile(client_data);
+  std::cout << "filetosearch>>" << filetosearch << "<<" << std::endl;
+  readFile(filetosearch.c_str(), &filecontent);
+  response += std::to_string(filecontent.length());              // fill the content-lenght header part
+  response += "\n\n";
+  response += filecontent;
+  return(response);
+}
+
 int main(int argc, char const *argv[])
 {
     int server_fd, new_socket; long valread;
@@ -51,13 +78,9 @@ int main(int argc, char const *argv[])
       std::cout << "port number needed" << std::endl;
       return(0);
     }
-    
-    // Only this line has been changed. Everything is same.
-    // std::string hello;
-    std::string hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: ";
-    // std::string hello = "HTTP/1.1 200 OK\nDate: Fri, 16 Mar 2018 17:36:27 GMT\nserver: myServerContent-Type: text/html;charset=UFT-8\nContent-Length: 1846\n\n<?xml>\n<!DOCTYPE html>\n<html>\n<body>\n\n<h1>My First Heading</h1>\n<p>My first paragraph.</p>\n\n</body>\n</html>\n";
+    std::string filetosearch;   
+    std::string response = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: ";
  
-    // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("In socket");
@@ -100,43 +123,30 @@ int main(int argc, char const *argv[])
         break;
       }
     }
+
+
     std::string filename;
     std::string filecontent;
     while(1)
     {
-        filename="root";
         printf("\n+++++++ Waiting for new connection on %d port ++++++++\n\n", port);
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
         {
             perror("In accept");
             exit(EXIT_FAILURE);
         }
-        hello = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: ";
+        // response = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: ";
         
-        char buffer[30000] = {0};
-        valread = read( new_socket , buffer, 30000);
-        printf(">>%s<<\n",buffer);
+        char client_data[30000] = {0};
+        valread = read( new_socket , client_data, 30000);
+        printf(">>%s<<\n",client_data);
         std::string file;
-        // file contain the word after GET /
 
-        for (int i = 4; i < valread; i++)
-        {
-            if (buffer[i] == ' ')
-                break;
-            file += buffer[i];
-        }
-        printf("file>>%s<<\n",file.c_str());
-        filename += file;
-        printf("filename>>%s<<\n",filename.c_str());
-        readFile(filename.c_str(), &filecontent);
-          // return (1);
-        hello += std::to_string(filecontent.length());
-        hello += "\n\n";
-        hello += filecontent;
-        // hello = data_sender(file);
-        // send_parse(new_socket, buffer); -- > get 
-        write(new_socket , hello.c_str() , hello.length());
-        printf("------------------Hello message sent-------------------");
+        response = data_sender(client_data);
+        // response += std::to_string(filecontent.length());
+        std::cout << "<<< " << response << std::endl;
+        write(new_socket , response.c_str() , response.length());
+        // printf("------->   message sent : \n%s <-------", response.c_str());
         close(new_socket);
     }
     return 0;
