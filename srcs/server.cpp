8fa049f34018e049a8b7812f915fdc4fecf2dff1
JCluzet <jo@cluzet.fr>
2022-04-26 15:21:12 +0200

    push

--- srcs/server.cpp
+++ srcs/server.cpp
@@ -12,32 +12,44 @@
 #include <sys/stat.h>
 #include <fstream>
 
+#ifdef __APPLE__ 
+    int mac = 1;
+#endif
+#ifdef __linux__ 
+    mac = 0;
+#endif
+
 bool is_directory(std::string path)
 {
-    struct stat  file_stat; 
+    struct stat file_stat;
 
-    if (stat(path.c_str(),&file_stat) < 0) 
-        return(0);
+    if (stat(path.c_str(), &file_stat) < 0)
+        return (0);
     else
-        return(1);
+        return (1);
 }
 
-std::string findInHeader(std::string header, std::string s){
+std::string findInHeader(std::string header, std::string s)
+{
     size_t pos;
     std::string tmp;
-    if (!strcmp(s.c_str(), "File")){
+    if (!strcmp(s.c_str(), "File"))
+    {
         header.erase(0, 4);
         return header.substr(0, header.find(' '));
     }
-    if (!strcmp(s.c_str(), "Version")){
+    if (!strcmp(s.c_str(), "Version"))
+    {
         header.erase(0, 4);
         header.erase(0, header.find(' ') + 1);
         return header.substr(0, header.find('\n'));
     }
-    while ((pos = header.find("\n")) != std::string::npos){
-        tmp = header.substr(0,pos);
-        if (tmp.length() >= s.length() && !strncmp(s.c_str(), tmp.c_str(), s.length())){
-            tmp.erase(0, s.length()+2);
+    while ((pos = header.find("\n")) != std::string::npos)
+    {
+        tmp = header.substr(0, pos);
+        if (tmp.length() >= s.length() && !strncmp(s.c_str(), tmp.c_str(), s.length()))
+        {
+            tmp.erase(0, s.length() + 2);
             return tmp;
         }
         header.erase(0, pos + 1);
@@ -45,18 +57,20 @@ std::string findInHeader(std::string header, std::string s){
     return "";
 }
 
+bool readFile(std::string filename, std::string *fileContent)
+{
+    std::string s;
+    std::ifstream ifs(filename);
 
-bool    readFile(std::string filename, std::string *fileContent) {
-    std::string     s;
-    std::ifstream   ifs(filename);
-
-    if (!ifs) {
+    if (!ifs)
+    {
         std::cerr << "Not Found " << filename << "." << std::endl;
         *fileContent = "\n<!DOCTYPE html>\n\n<html>\n\n<body>\n  \n  <h1>ERROR 404</h1>\n    <p>File not found.</p>\n</body>\n\n</html>";
         return (1);
     }
     getline(ifs, s);
-    if (s == "") {
+    if (s == "")
+    {
         std::cerr << "Empty file." << std::endl;
         ifs.close();
         *fileContent = "\n";
@@ -64,7 +78,7 @@ bool    readFile(std::string filename, std::string *fileContent) {
         return (1);
     }
     *fileContent += s;
-    while(getline(ifs, s))
+    while (getline(ifs, s))
     {
         *fileContent += "\n";
         *fileContent += s;
@@ -89,10 +103,20 @@ std::string getContentType(std::string client_data)
         if (client_data[i] == 'A' && client_data[i + 1] == 'c' && client_data[i + 2] == 'c' && client_data[i + 3] == 'e' && client_data[i + 4] == 'p' && client_data[i + 5] == 't' && client_data[i + 6] == ':')
         {
             i += 8;
-            while (client_data[i] != ',')
+            std::string tmp = findInHeader(client_data, "File");
+            // std::cout << "tmp >> " << tmp << std::endl;
+            if (tmp[tmp.length() - 1] == 'g' && tmp[tmp.length() - 2] == 'v' && tmp[tmp.length() - 3] == 's') // --> if it's a svg file, use image/svg+xml content type
             {
-                content_type += client_data[i];
-                i++;
+                content_type = "image/svg+xml";
+                return (content_type);
+            }
+            else
+            {
+                while (client_data[i] != ',')
+                {
+                    content_type += client_data[i];
+                    i++;
+                }
             }
             break;
         }
@@ -103,58 +127,57 @@ std::string getContentType(std::string client_data)
 
 std::string getFile(std::string client_data)
 {
-  std::string file;
-  for (size_t i = 4; i < client_data.length(); i++)
-  {
-    if (client_data[i] == ' ')
-      break;
-    file += client_data[i];
-  }
-  return (file);
+    std::string file;
+    for (size_t i = 4; i < client_data.length(); i++)
+    {
+        if (client_data[i] == ' ')
+            break;
+        file += client_data[i];
+    }
+    return (file);
 }
 
 std::string getHeader(std::string client_data)
 {
-  std::string response = "HTTP/1.1 200 OK";
-  response += "\nContent-Type: ";
-  response += getContentType(client_data);
-  // response += "text/html"; // -->> need to detect the content type
-  response += "\nContent-Length: ";
-  return(response);
+    std::string response = "HTTP/1.1 200 OK";
+    response += "\nContent-Type: ";
+    response += getContentType(client_data);
+    // response += "text/html"; // -->> need to detect the content type
+    response += "\nContent-Length: ";
+    return (response);
 }
 
 #include <sys/types.h>
 
 std::string set_default_page(std::string filetosearch, std::string client_data)
 {
-        std::cout << "present" << std::endl;
-    (void)client_data;          // ---> need to check if the file is a directory and then set the default page
-    if(filetosearch[filetosearch.length() - 1] == '/')
+    std::cout << "present" << std::endl;
+    (void)client_data; // ---> need to check if the file is a directory and then set the default page
+    if (filetosearch[filetosearch.length() - 1] == '/')
     {
-      filetosearch += "index.html";                                             // <-- replace here the default page in config file
-      std::cout << "setting DEFAULT to index.html" << std::endl;
+        filetosearch += "index.html"; // <-- replace here the default page in config file
+        std::cout << "setting DEFAULT to index.html" << std::endl;
     }
     return (filetosearch);
 }
 
-
 std::string data_sender(std::string client_data)
 {
-  std::string response;
-  std::string filecontent;
-  response = getHeader(client_data);
-  std::string filetosearch = "root" + findInHeader(client_data, "File");                       // <-- replace "root" here with config file
-//   if (is_directory(filetosearch))
-//   {
-//     filetosearch += "/";
-//   }
-  filetosearch = set_default_page(filetosearch, client_data);
-  std::cout << "filetosearch>>" << filetosearch << "<<" << std::endl;
-  readFile(filetosearch.c_str(), &filecontent);
-  response += std::to_string(filecontent.length());              // fill the content-lenght header part
-  response += "\n\n";
-  response += filecontent;
-  return(response);
+    std::string response;
+    std::string filecontent;
+    response = getHeader(client_data);
+    std::string filetosearch = "root" + findInHeader(client_data, "File"); // <-- replace "root" here with config file
+                                                                           //   if (is_directory(filetosearch))
+                                                                           //   {
+                                                                           //     filetosearch += "/";
+                                                                           //   }
+    filetosearch = set_default_page(filetosearch, client_data);
+    std::cout << "filetosearch>>" << filetosearch << "<<" << std::endl;
+    readFile(filetosearch.c_str(), &filecontent);
+    response += std::to_string(filecontent.length()); // fill the content-lenght header part
+    response += "\n\n";
+    response += filecontent;
+    return (response);
 }
 
 int main(int argc, char const *argv[])
@@ -163,30 +186,28 @@ int main(int argc, char const *argv[])
     long valread;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    if(argc != 2)
+    if (argc != 2)
     {
-      std::cout << "port number needed" << std::endl;
-      return(0);
+        std::cout << "port number needed" << std::endl;
+        return (0);
     }
-    std::string filetosearch;   
+    std::string filetosearch;
     std::string response = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: ";
- 
+
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("In socket");
         exit(EXIT_FAILURE);
     }
-    
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     int port = atoi(argv[1]);
-    address.sin_port = htons( port );
-    
+    address.sin_port = htons(port);
+
     memset(address.sin_zero, '\0', sizeof address.sin_zero);
-    
-    
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
         perror("In bind");
         exit(EXIT_FAILURE);
@@ -197,44 +218,45 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
     std::string test;
-    printf ("Want to open page on port %d ?\n (y/n)", port);
-    while(1)
+    printf("Want to open page on port %d ?\n (y/n)", port);
+    while (1)
     {
-      std::cin >> test;
-      if (test == "y")
-      {
-        std::cout << "Opening page on port " << port << std::endl;
-        system(("open http://localhost:" + std::to_string(port)).c_str());     // --> mac
-        // system(("xdg-open http://localhost:" + std::to_string(port)).c_str()); --> linux
-        break;
-      }
-      if (test == "n")
-      {
-        break;
-      }
+        std::cin >> test;
+        if (test == "y")
+        {
+            std::cout << "Opening page on port " << port << std::endl;
+            if (mac == 1)
+                system(("open http://localhost:" + std::to_string(port)).c_str()); // --> mac
+            else
+                system(("xdg-open http://localhost:" + std::to_string(port)).c_str());
+        }
+        if (test == "n")
+        {
+            break;
+        }
     }
-
 
     std::string filename;
     std::string filecontent;
-    while(1)
+    while (1)
     {
         printf("\n+++++++ Waiting for new connection on %d port ++++++++\n\n", port);
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
         {
             perror("In accept");
             exit(EXIT_FAILURE);
         }
-        
+
         char client_data[30000] = {0};
-        valread = read( new_socket , client_data, 30000);
-        printf("RECEIVING:\n%s\n",client_data);
+        valread = read(new_socket, client_data, 30000);
+        printf("RECEIVING:\n%s\n", client_data);
         std::string file;
-        (void)valread; // --> ???? 
+        (void)valread; // --> ????
 
         response = data_sender(client_data);
-        std::cout << "\n\nOUR RESPONSE: " << std::endl << response << std::endl;
-        write(new_socket , response.c_str() , response.length());
+        std::cout << "\n\nOUR RESPONSE: " << std::endl
+                  << response << std::endl;
+        write(new_socket, response.c_str(), response.length());
         close(new_socket);
     }
     return 0;
