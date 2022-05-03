1b837e44ab51de0fe72cb9f403ed4fab5c82dee9
JCluzet <jo@cluzet.fr>
2022-05-03 21:29:43 +0200

    push

--- www/action_page.php
+++ www/action_page.php
@@ -1,3 +1,3 @@
-<!-- # output the 2 arguments in the POST request -->
-echo "Hello " . $_GET["lname"] . "!<br>";
-echo "You are " . $_GET["fname"] . " years old.";
+<?php
+echo "Bonjour " . $_GET['lname'] . " " . $_GET['fname'] . " !";
+?>
