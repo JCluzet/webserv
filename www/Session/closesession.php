7af16855ce2b9ed2679629e2a45d8d591e4133e7
JCluzet <jo@cluzet.fr>
2022-05-25 04:49:42 +0200

    beautifyclosessession

--- www/Session/closesession.php
+++ www/Session/closesession.php
@@ -2,6 +2,25 @@
 session_start();
 unset($_SESSION);
 session_destroy();
-echo "Session is now destroyed!";
-echo '<br><br><br /><a href="/">Back to HomePage</a>';
 ?>
+<body>
+    <section class="notFound">
+    <link rel="stylesheet" href="/style/style.css">
+        <div class="text">
+            <h1><a href="/">HomePage</a></h1>
+            <h3>WEBSERV 1.0</h3>
+        <div class="img">
+            <img src="http://i.stack.imgur.com/SBv4T.gif" alt="this slowpoke moves"  width="250" />
+            <br><br>
+            <br><br>
+            <br><br>
+<?php
+
+echo 'Session is now closed!';
+
+?>
+<br><br><br>
+</div>
+</section>
+</body>
+</html>
