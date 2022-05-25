211b29880d2cb3d5e01efd90247fa2c1f9b48dab
JCluzet <jo@cluzet.fr>
2022-05-25 04:17:07 +0200

    push

--- www/Session/startsession.php
+++ www/Session/startsession.php
@@ -2,7 +2,7 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
     $_SESSION["newsession"] = "newsession";
-    $_SESSION["name"] = "test";
+    // $_SESSION["name"] = "test";
 }
 ?>
 <body>
